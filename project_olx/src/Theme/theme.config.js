1bae2f2d8f4d1b811589ddad9a68e7445c68ece1
Prashant3216 <pv3216@gmail.com>
2022-08-25 22:54:08 +0530

    pushing day-2

---
+++ project_olx/src/Theme/theme.config.js
@@ -0,0 +1,21 @@
+
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme=extendTheme({
+    colors:{
+        primary:"rgba(0,47,52,.06)",
+        secondary: "#002f34",
+        searchTabBg:"white",
+        searchBorder:"#23e8e1",
+        // shadow:"rgba(0, 0, 0, 0.15) 0px 3px 3px 0px"
+    },
+    fontFamily:{textType:"Roboto,Arial,Helvetica,sans-serif"}
+
+})
+
+// export const primary = "rgba(0,47,52,.06)";
+// export const secondary = "#002f34";
+
+// export const searchTabBg="white"
+export const shadow="rgba(0, 0, 0, 0.15) 0px 3px 3px 0px"
+// export const searchBorder="#23e8e1"
---
+++ project_olx/src/components/Login.jsx
@@ -0,0 +1,5 @@
+import { NavLink } from "react-router-dom";
+
+export default function Login() {
+  return <div>Login</div>;
+}
